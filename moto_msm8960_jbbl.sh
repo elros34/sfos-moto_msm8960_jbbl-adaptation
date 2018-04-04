a7af631cf5de0a43d22ebf2c01b0253d175a1610
elros34 <elros34@users.noreply.github.com>
2018-04-04 19:24:48 +0000

    Update to 2.1.4.14

--- moto_msm8960_jbbl.sh
+++ moto_msm8960_jbbl.sh
@@ -2,22 +2,34 @@
 set -e
 set -x
 
+PKG_DIR="/usr/share/sfos-moto_msm8960_jbbl-adaptation"
+
+mkdir -p $PKG_DIR/backup/
+
 # system mount
 echo "Removing remount option from system.mount"
+/bin/cp -f /lib/systemd/system/system.mount $PKG_DIR/backup/
 sed -i "s|Options=ro,remount|Options=ro|" /lib/systemd/system/system.mount
 
 # camera 
 echo "Patching jolla-camera"
-/bin/cp -f /usr/share/sfos-moto_msm8960_jbbl-adaptation/patches/jolla-camera.patch /usr/lib/qt5/qml/com/jolla/camera/capture/
-cd /usr/lib/qt5/qml/com/jolla/camera/capture/
+CAMERA_DIR="/usr/lib/qt5/qml/com/jolla/camera/capture"
+/bin/cp -f $CAMERA_DIR/CaptureOverlay.qml $PKG_DIR/backup/
+/bin/cp -f $CAMERA_DIR/CaptureView.qml $PKG_DIR/backup/
+
+/bin/cp -f $PKG_DIR/patches/jolla-camera.patch $CAMERA_DIR
+cd $CAMERA_DIR
 patch -p1 < jolla-camera.patch
 cd -
 
 # APN settings
-echo "Changing /var/lib/ofono ownership"
-chown -R radio:radio /var/lib/ofono
+if [ "$(ls -ld /var/lib/ofono | cut -d" " -f3,4)" != "radio radio" ]; then
+	echo "Changing /var/lib/ofono ownership"
+	chown -R radio:radio /var/lib/ofono
+fi
 
 # Make sure system partition is mounted before droid-hal-init
+/bin/cp -f /lib/systemd/system/local-fs.target $PKG_DIR/backup/
 cat << EOF >> /lib/systemd/system/local-fs.target
 
 [Install]
@@ -26,9 +38,6 @@ EOF
 
 sed -i "s|WantedBy=local-fs.target|RequiredBy=local-fs.target|g" /lib/systemd/system/system.mount
 
-# Delay sensorfwd start
-sed -i "s|After=dbus.socket|After=dbus.socket sys-devices-platform-SENSOR.3.device\nRequires=sys-devices-platform-SENSOR.3.device|g" /lib/systemd/system/sensorfwd.service
-
 # keyboard layout, mount-sd.sh
 /bin/cp -rf /usr/share/sfos-moto_msm8960_jbbl-adaptation/sparse/* /
 
@@ -36,5 +45,19 @@ sed -i "s|After=dbus.socket|After=dbus.socket sys-devices-platform-SENSOR.3.devi
 #ln -fs /lib/systemd/system/zramswap.service /lib/systemd/system/multi-user.target.wants/zramswap.service
 
 # Add repo used in image creation with modified fingerterm, warehouse and sdcard-moded
-echo "Adding http://repo.merproject.org/obs/home:/elros34:/sailfishapps/sailfishos_2.1.3.7/ repo"
-ssu ar elros34-sailfishapps http://repo.merproject.org/obs/home:/elros34:/sailfishapps/sailfishos_2.1.3.7/
+echo "Adding http://repo.merproject.org/obs/home:/elros34:/sailfishapps/sailfishos_2.1.4.14/ repo"
+ssu ar elros34-sailfishapps http://repo.merproject.org/obs/home:/elros34:/sailfishapps/sailfishos_2.1.4.14/
+
+# Unlock screen on kbd slide 
+mcetool --set-filter-lid-with-als=disabled
+mcetool --set-kbd-slide-open-trigger=always
+mcetool --set-kbd-slide-open-action=tkunlock
+mcetool --set-kbd-slide-close-trigger=after-open
+mcetool --set-kbd-slide-close-action=tklock
+
+# Don't enable display when headphones are connected/disconnected or camera and volume buttons are pressed
+mcetool --set-exception-length-jack-in=0
+mcetool --set-exception-length-jack-out=0
+mcetool --set-exception-length-camera=0
+mcetool --set-exception-length-volume=0
+
--- rpm/sfos-moto_msm8960_jbbl-adaptation.spec
+++ rpm/sfos-moto_msm8960_jbbl-adaptation.spec
@@ -1,6 +1,6 @@
 Name:       sfos-moto_msm8960_jbbl-adaptation
 Summary:    Bunch of dirty hacks for moto_msm8960_jbbl
-Version:    0.0.1
+Version:    0.0.2
 Release:    1
 Group:      Qt/Qt
 License:    LICENSE
