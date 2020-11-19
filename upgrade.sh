227405860a075086e799dcb39a8e6ea04390e7d1
elros34 <elros34@users.noreply.github.com>
2020-11-19 22:38:11 +0100

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -10,18 +10,24 @@ fi
 
 export CURRENT_RELEASE="$(ssu re | pcregrep -o1 '([\d\.]+)')" 
 
-if [[ "$1" == "--local="* ]]; then
-    export NEXT_RELEASE="$(cut -d= -f2 <<< $1)"
-    createrepo_c --update /droid-local-repo
-    ssu ar droid-local-repo file:///droid-local-repo
-    zypper ref droid-local-repo
-    zypper --non-interactive in --force --from droid-local-repo sfos-moto_msm8960_jbbl-adaptation
+if [ $# -ge 1 ]; then
+    if [[ "$1" == "--local="* ]]; then
+        export NEXT_RELEASE="$(cut -d= -f2 <<< $1)"
+        createrepo_c --update /droid-local-repo
+        ssu ar droid-local-repo file:///droid-local-repo
+        zypper ref droid-local-repo
+        zypper --non-interactive in --force --from droid-local-repo sfos-moto_msm8960_jbbl-adaptation
 
-    /usr/share/sfos-moto_msm8960_jbbl-adaptation/real-upgrade.sh
+        /usr/share/sfos-moto_msm8960_jbbl-adaptation/real-upgrade.sh
     
-    #ssu rr droid-local-repo
-    exit 0
+        #ssu rr droid-local-repo
+        exit 0
+    else
+        echo "Incorrect argument" 
+        exit 1
+    fi
 fi
+
 
 release2num() {
     echo "$1" | awk -v FS=. '{print $1$2$3}'
@@ -31,7 +37,7 @@ release2num() {
 # Stop releases without minor
 # cloudfire blocks it..
 #STOP_RELEASES="$(curl https://jolla.zendesk.com/hc/en-us/articles/201836347 2>/dev/null | pcregrep -o1 '<li>(\d\.\d\.\d).*</li>')"
-STOP_RELEASES="1.0.2, 1.1.2, 1.1.7, 1.1.9, 2.0.0, 2.2.0, 3.0.0, 3.2.0"
+STOP_RELEASES="1.0.2 1.1.2 1.1.7 1.1.9 2.0.0 2.2.0 3.0.0 3.2.0"
 AVAILABLE_RELEASES="$(curl http://repo.merproject.org/obs/nemo:/testing:/hw:/motorola:/moto_msm8960_jbbl/ 2>/dev/null | pcregrep -o1 '\"sailfishos_([\d\.]+)')"
 CURRENT_RELEASE_NUM="$(release2num $CURRENT_RELEASE)"
 NEXT_RELEASE="$(echo $AVAILABLE_RELEASES  | tr ' ' '\n' | tail -n1)"
